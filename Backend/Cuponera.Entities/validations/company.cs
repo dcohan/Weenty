3d48086ed851f3218a68957443d29569b8498208
dcohan <dcohan@vmbc.com>
2014-11-06 22:36:44 -0300

    added data annotation for category table

---
+++ Backend/Cuponera.Entities/validations/company.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cuponera.Entities
+{
+    [MetadataType(typeof(companyMetadata))]
+    public partial class company
+    {
+
+    }
+    public class companyMetadata
+    {
+        [Required(ErrorMessage = "Debe ingresar un nombre de compañia.")]
+        [MaxLength(100, ErrorMessage = "El nombre de la compañia debe tener como máximo de 100 caracteres."), MinLength(10, ErrorMessage = "El nombre de la compañia debe tener como mínimo 10 caracteres.")]
+        public string Name { get; set; }
+    }
+}
