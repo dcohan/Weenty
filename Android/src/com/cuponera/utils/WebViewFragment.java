09cf29574f198eb7e9426b60d2710c85dec06151
Juan Pablo Cuervo <juanpi.cuervo@hotmail.com>
2014-12-29 18:16:05 -0300

    Fix uploading images

--- Android/src/com/cuponera/utils/WebViewFragment.java
+++ Android/src/com/cuponera/utils/WebViewFragment.java
@@ -1,65 +1,122 @@
 package com.cuponera.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 import com.cuponera.BaseFragment;
 import com.cuponera.R;
 
-
 public abstract class WebViewFragment extends BaseFragment {
-	
+
+	private ValueCallback<Uri> mUploadMessage;
+	private final static int FILECHOOSER_RESULTCODE = 1;
 	protected WebView mWebView;
-	
+
 	@Override
 	protected int getLayout() {
 		return R.layout.fragment_web_view;
 	}
-	
+
 	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		mWebView = mViewProxy.findWebView(R.id.webView);
-		
+
 		setSettings();
-		
+
 		mWebView.loadUrl(getUrl());
-		
+
 	}
-	
+
 	@SuppressLint("SetJavaScriptEnabled")
 	private void setSettings() {
 		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setAllowFileAccess(true);
 		mWebView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 		mWebView.getSettings().setLoadsImagesAutomatically(true);
-		
-		mWebView.setWebViewClient(new WebViewClient() {
-		    @Override
-		    public boolean shouldOverrideUrlLoading(WebView view, String url) {
-		    	mWebView.loadUrl(url);
-		        return true;
-		    }
-		    
-		    @Override
-		    public void onPageStarted(WebView view, String url, Bitmap favicon) {
-		    	super.onPageStarted(view, url, favicon);
-		    	getBaseActivity().showLoading();
-		    }
-		    
-		    @Override
-		    public void onPageFinished(WebView view, String url) {
-		    	super.onPageFinished(view, url);
-		    	getBaseActivity().hideLoading();
-		    }
-		    
+
+		mWebView.setWebChromeClient(new WebChromeClient() {
+			public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+
+				mUploadMessage = uploadMsg;
+				Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+				i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("image/*");
+				startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+
+			}
+
+			public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+				mUploadMessage = uploadMsg;
+				Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+				i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("*/*");
+				startActivityForResult(Intent.createChooser(i, "File Browser"), FILECHOOSER_RESULTCODE);
+			}
+
+			public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+				mUploadMessage = uploadMsg;
+				Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+				i.addCategory(Intent.CATEGORY_OPENABLE);
+				i.setType("image/*");
+				startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+
+			}
+
 		});
+		mWebView.setWebViewClient(new WebViewClient() {
+			@Override
+			public boolean shouldOverrideUrlLoading(WebView view, String url) {
+				mWebView.loadUrl(url);
+				return true;
+			}
+
+			@Override
+			public void onPageStarted(WebView view, String url, Bitmap favicon) {
+				super.onPageStarted(view, url, favicon);
+				getBaseActivity().showLoading();
+			}
+
+			@Override
+			public void onPageFinished(WebView view, String url) {
+				super.onPageFinished(view, url);
+				getBaseActivity().hideLoading();
+			}
+
+		});
+
 	}
-	
+
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+		if (requestCode == FILECHOOSER_RESULTCODE) {
+			if (null == mUploadMessage)
+				return;
+			Uri result = intent == null || resultCode != getActivity().RESULT_OK ? null : intent.getData();
+			String imageUrl;
+			try {
+				imageUrl = URLDecoder.decode(result.toString(), "UTF-8");
+				result = Uri.parse(imageUrl);
+				mUploadMessage.onReceiveValue(result);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			}
+
+			mUploadMessage = null;
+		}
+	}
+
 	protected abstract String getUrl();
 
 }
