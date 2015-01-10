ab8ca764c99afbec2bdc6af5872cdfa462e7ddc8
unknown <juanpi.cuervo@hotmail.com>
2015-01-10 20:06:46 -0300

    termns and conditions and webconfig production, sory guys didint want to create new one, then we will comment the lines. fuck you all

---
+++ Backend/Cuponera.WebSite/Controllers/TermsAndConditionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cuponera.WebSite.Controllers
+{
+    public class TermsAndConditionsController : Controller
+    {
+        //
+        // GET: TermnsAndConditions
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
