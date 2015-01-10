874773811a92efe548c853eddb2d2bfe2c274baa
Juan Pablo Cuervo <juanpi.cuervo@hotmail.com>
2015-01-10 20:46:32 -0300

    last style. need help with stores procedures are not working good

--- Android/src/com/cuponera/search/CustomPagerAdapter.java
+++ Android/src/com/cuponera/search/CustomPagerAdapter.java
@@ -19,10 +19,12 @@ public class CustomPagerAdapter extends PagerAdapter {
 
 	List<Category> items;
 	LayoutInflater inflater;
+	private Context context;
 
 	public CustomPagerAdapter(Context context, ArrayList<Category> items) {
 		this.inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		this.items = items;
+		this.context = context;
 	}
 
 	@Override
@@ -48,7 +50,9 @@ public class CustomPagerAdapter extends PagerAdapter {
 
 		TextView topTextItem = (TextView) itemView.findViewById(R.id.topText);
 		if (position + 1 == getCount()) {
-			topTextItem.setCompoundDrawablesWithIntrinsicBounds(null, null, null, null);
+			topTextItem.setCompoundDrawablesWithIntrinsicBounds(context.getResources().getDrawable(R.drawable.ic_action_arrow_left), null, null, null);
+		} else if (position == 0) {
+			topTextItem.setCompoundDrawablesWithIntrinsicBounds(null, null, context.getResources().getDrawable(R.drawable.ic_action_arrow_right), null);
 		}
 		Category c = items.get(position);
 
